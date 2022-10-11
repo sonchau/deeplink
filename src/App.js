c1f4b1d3912a685d8fc8e23261a788fb8ae7a0c7
Son Chau <sonchau24@yahoo.com>
2022-10-11 15:07:08 +1100

    first commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useEffect } from "react";
+import { isAndroid, isIOS } from "react-device-detect";
+
+const App = () => {
+  useEffect(() => {
+    if (isAndroid) {
+      const url =
+        "intent://instagram.com/#Intent;scheme=https;package=com.instagram.android;end";
+
+      window.location.replace(url);
+    } else if (isIOS) {
+      window.location.replace("instagram://");
+
+      setTimeout(() => {
+        window.location.replace(
+          "https://apps.apple.com/us/app/instagram/id389801252"
+        );
+      }, 10000);
+    } else {
+      window.location.replace("https://instagram.com");
+    }
+  }, []);
+
+  return (
+    <div className="App">
+      <div>
+        If you have not been automatically redirected, click on the following link:
+      </div>
+      {isAndroid ? (
+        <a href="https://play.google.com/store/apps/details?id=com.instagram.android">
+          Open Android app
+        </a>
+      ) : isIOS ? (
+        <a href="https://apps.apple.com/us/app/instagram/id389801252">
+          Open iOS app
+        </a>
+      ) : (
+        <a href="https://instagram.com">Open Web app</a>
+      )}
+    </div>
+  );
+};
+
+export default App;
